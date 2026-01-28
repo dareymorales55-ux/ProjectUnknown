8b036884309f48ecc5f070d5327b057375837a22
dareymorales55-ux <dareymorales55@gmail.com>
2026-01-27 19:45:44 -0800

    Set up ProjectUnknown plugin structure
    Initialize ProjectUnknown plugin and register event listeners.

--- src/main/java/com/deejay/projectunknown/ProjectUnknown.java
+++ src/main/java/com/deejay/projectunknown/ProjectUnknown.java
@@ -1,1 +1,45 @@
+package com.deejay.projectunknown;
 
+import com.deejay.projectunknown.listeners.JoinListener;
+import com.deejay.projectunknown.listeners.CompassListener;
+import com.deejay.projectunknown.reveal.RevealManager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ProjectUnknown extends JavaPlugin {
+
+    private static ProjectUnknown instance;
+    private RevealManager revealManager;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        this.revealManager = new RevealManager(this);
+
+        getServer().getPluginManager().registerEvents(
+                new JoinListener(this),
+                this
+        );
+
+        getServer().getPluginManager().registerEvents(
+                new CompassListener(this),
+                this
+        );
+
+        getLogger().info("ProjectUnknown enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("ProjectUnknown disabled");
+    }
+
+    public static ProjectUnknown getInstance() {
+        return instance;
+    }
+
+    public RevealManager getRevealManager() {
+        return revealManager;
+    }
+}
