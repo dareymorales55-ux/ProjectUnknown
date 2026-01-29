c2bea516d92dd4a68698c723dd44ab64b4847531
dareymorales55-ux <dareymorales55@gmail.com>
2026-01-28 16:17:48 -0800

    Implement player configuration handling
    Added player configuration management with YAML file support.

--- src/main/java/com/deejay/projectunknown/ProjectUnknown.java
+++ src/main/java/com/deejay/projectunknown/ProjectUnknown.java
@@ -4,16 +4,28 @@ import com.deejay.projectunknown.listeners.JoinListener;
 import com.deejay.projectunknown.listeners.CompassListener;
 import com.deejay.projectunknown.reveal.RevealManager;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ProjectUnknown extends JavaPlugin {
 
     private static ProjectUnknown instance;
     private RevealManager revealManager;
+    
+    // Config file fields
+    private File playerConfigFile;
+    private FileConfiguration playerConfig;
 
     @Override
     public void onEnable() {
         instance = this;
+
+        // Initialize custom config first so listeners can use it
+        createPlayerConfig();
 
         this.revealManager = new RevealManager(this);
 
@@ -32,7 +44,33 @@ public class ProjectUnknown extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        savePlayerConfig(); // Auto-save on shutdown
         getLogger().info("ProjectUnknown disabled");
+    }
+
+    // This method fixes the error in JoinListener.java:[31,15]
+    public FileConfiguration getPlayerConfig() {
+        return this.playerConfig;
+    }
+
+    // This method fixes the error in JoinListener.java:[32,15]
+    public void savePlayerConfig() {
+        try {
+            getPlayerConfig().save(playerConfigFile);
+        } catch (IOException e) {
+            getLogger().severe("Could not save players.yml!");
+        }
+    }
+
+    // Sets up the physical players.yml file in the plugin folder
+    private void createPlayerConfig() {
+        playerConfigFile = new File(getDataFolder(), "players.yml");
+        if (!playerConfigFile.exists()) {
+            playerConfigFile.getParentFile().mkdirs();
+            saveResource("players.yml", false);
+        }
+
+        playerConfig = YamlConfiguration.loadConfiguration(playerConfigFile);
     }
 
     public static ProjectUnknown getInstance() {
