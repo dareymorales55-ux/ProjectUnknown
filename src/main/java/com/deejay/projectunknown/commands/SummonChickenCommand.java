b3b419afccbfe2075c54f52f2e5f8ed2c1f61db8
dareymorales55-ux <dareymorales55@gmail.com>
2026-01-28 19:05:51 -0800

    Refactor chicken summon command to use plugin instance
    Refactor SummonChickenCommand to use plugin instance for UnknownChicken spawn.

--- src/main/java/com/deejay/projectunknown/commands/SummonChickenCommand.java
+++ src/main/java/com/deejay/projectunknown/commands/SummonChickenCommand.java
@@ -1,28 +1,27 @@
 package com.deejay.projectunknown.commands;
 
+import com.deejay.projectunknown.ProjectUnknown;
 import com.deejay.projectunknown.mobs.UnknownChicken;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 
 public class SummonChickenCommand implements CommandExecutor {
+    private final ProjectUnknown plugin;
+
+    public SummonChickenCommand(ProjectUnknown plugin) {
+        this.plugin = plugin;
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-        if (!(sender instanceof Player)) {
-            sender.sendMessage("Only players can use this command.");
-            return true;
-        }
-
+        if (!(sender instanceof Player)) return true;
         Player player = (Player) sender;
 
-        UnknownChicken.spawn(player.getLocation());
+        // Create a new instance or use one from plugin to call non-static spawn
+        new UnknownChicken(plugin).spawn(player.getLocation());
 
         player.sendMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + "The Unknown Chicken has been summoned.");
-
         return true;
     }
 }
