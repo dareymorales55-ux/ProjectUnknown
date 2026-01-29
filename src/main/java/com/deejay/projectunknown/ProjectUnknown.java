efe88465e1bd9bdee9fac700350fed00c3810863
dareymorales55-ux <dareymorales55@gmail.com>
2026-01-28 19:02:43 -0800

    Refactor listener registration and command execution

--- src/main/java/com/deejay/projectunknown/ProjectUnknown.java
+++ src/main/java/com/deejay/projectunknown/ProjectUnknown.java
@@ -2,66 +2,36 @@ package com.deejay.projectunknown;
 
 import com.deejay.projectunknown.commands.GiveCompassCommand;
 import com.deejay.projectunknown.commands.SummonChickenCommand;
-import com.deejay.projectunknown.listeners.JoinListener;
-import com.deejay.projectunknown.listeners.CompassListener;
-import com.deejay.projectunknown.listeners.EggListener;
+import com.deejay.projectunknown.listeners.*;
 import com.deejay.projectunknown.mobs.UnknownChicken;
 import com.deejay.projectunknown.reveal.RevealManager;
-
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import java.io.File;
 import java.io.IOException;
 
 public class ProjectUnknown extends JavaPlugin {
-
     private static ProjectUnknown instance;
     private RevealManager revealManager;
-
-    // players.yml
     private File playerConfigFile;
     private FileConfiguration playerConfig;
 
     @Override
     public void onEnable() {
         instance = this;
-
-        // Create players.yml FIRST
         createPlayerConfig();
-
-        // Reveal system (handles ALL reveal logic)
         this.revealManager = new RevealManager(this);
 
-        // =========================
-        // Listeners
-        // =========================
-        getServer().getPluginManager().registerEvents(
-                new JoinListener(this),
-                this
-        );
+        // Register all listeners
+        getServer().getPluginManager().registerEvents(new JoinListener(this), this);
+        getServer().getPluginManager().registerEvents(new CompassListener(this), this);
+        getServer().getPluginManager().registerEvents(new EggListener(this), this);
+        getServer().getPluginManager().registerEvents(new BellListener(this), this);
+        getServer().getPluginManager().registerEvents(new UnknownChicken(this), this);
 
-        getServer().getPluginManager().registerEvents(
-                new CompassListener(this),
-                this
-        );
-
-        getServer().getPluginManager().registerEvents(
-                new EggListener(this),
-                this
-        );
-
-        getServer().getPluginManager().registerEvents(
-                new UnknownChicken(this),
-                this
-        );
-
-        // =========================
-        // Commands
-        // =========================
         getCommand("givecompass").setExecutor(new GiveCompassCommand());
-        getCommand("summonchicken").setExecutor(new SummonChickenCommand());
+        getCommand("summonchicken").setExecutor(new SummonChickenCommand(this));
 
         getLogger().info("ProjectUnknown enabled");
     }
@@ -69,45 +39,22 @@ public class ProjectUnknown extends JavaPlugin {
     @Override
     public void onDisable() {
         savePlayerConfig();
-        getLogger().info("ProjectUnknown disabled");
     }
 
-    /* =========================
-       players.yml access
-       ========================= */
-
-    public FileConfiguration getPlayerConfig() {
-        return this.playerConfig;
-    }
+    public FileConfiguration getPlayerConfig() { return this.playerConfig; }
+    public RevealManager getRevealManager() { return revealManager; }
+    public static ProjectUnknown getInstance() { return instance; }
 
     public void savePlayerConfig() {
-        try {
-            playerConfig.save(playerConfigFile);
-        } catch (IOException e) {
-            getLogger().severe("Could not save players.yml!");
-        }
+        try { playerConfig.save(playerConfigFile); } catch (IOException e) { e.printStackTrace(); }
     }
 
     private void createPlayerConfig() {
         playerConfigFile = new File(getDataFolder(), "players.yml");
-
         if (!playerConfigFile.exists()) {
             playerConfigFile.getParentFile().mkdirs();
             saveResource("players.yml", false);
         }
-
         playerConfig = YamlConfiguration.loadConfiguration(playerConfigFile);
-    }
-
-    /* =========================
-       Getters
-       ========================= */
-
-    public static ProjectUnknown getInstance() {
-        return instance;
-    }
-
-    public RevealManager getRevealManager() {
-        return revealManager;
     }
 }
