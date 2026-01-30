bcd2db97af301628e00e18f56bd8bd4acb336501
dareymorales55-ux <dareymorales55@gmail.com>
2026-01-29 16:07:38 -0800

    Refactor ProjectUnknown class and improve config handling

--- src/main/java/com/deejay/projectunknown/ProjectUnknown.java
+++ src/main/java/com/deejay/projectunknown/ProjectUnknown.java
@@ -1,5 +1,6 @@
 package com.deejay.projectunknown;
 
+import com.deejay.projectunknown.commands.GiveBellCommand;
 import com.deejay.projectunknown.commands.GiveCompassCommand;
 import com.deejay.projectunknown.commands.SummonChickenCommand;
 import com.deejay.projectunknown.listeners.*;
@@ -8,29 +9,39 @@ import com.deejay.projectunknown.reveal.RevealManager;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import java.io.File;
 import java.io.IOException;
 
 public class ProjectUnknown extends JavaPlugin {
+
     private static ProjectUnknown instance;
     private RevealManager revealManager;
+
+    // players.yml
     private File playerConfigFile;
     private FileConfiguration playerConfig;
 
     @Override
     public void onEnable() {
         instance = this;
+
+        // Create players.yml
         createPlayerConfig();
+
+        // Reveal system
         this.revealManager = new RevealManager(this);
 
-        // Register all listeners
+        // Listeners
         getServer().getPluginManager().registerEvents(new JoinListener(this), this);
         getServer().getPluginManager().registerEvents(new CompassListener(this), this);
         getServer().getPluginManager().registerEvents(new EggListener(this), this);
         getServer().getPluginManager().registerEvents(new BellListener(this), this);
         getServer().getPluginManager().registerEvents(new UnknownChicken(this), this);
 
+        // Commands
         getCommand("givecompass").setExecutor(new GiveCompassCommand());
+        getCommand("givebell").setExecutor(new GiveBellCommand(this));
         getCommand("summonchicken").setExecutor(new SummonChickenCommand(this));
 
         getLogger().info("ProjectUnknown enabled");
@@ -39,22 +50,45 @@ public class ProjectUnknown extends JavaPlugin {
     @Override
     public void onDisable() {
         savePlayerConfig();
+        getLogger().info("ProjectUnknown disabled");
     }
 
-    public FileConfiguration getPlayerConfig() { return this.playerConfig; }
-    public RevealManager getRevealManager() { return revealManager; }
-    public static ProjectUnknown getInstance() { return instance; }
+    /* =========================
+       players.yml
+       ========================= */
+
+    public FileConfiguration getPlayerConfig() {
+        return playerConfig;
+    }
 
     public void savePlayerConfig() {
-        try { playerConfig.save(playerConfigFile); } catch (IOException e) { e.printStackTrace(); }
+        try {
+            playerConfig.save(playerConfigFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void createPlayerConfig() {
         playerConfigFile = new File(getDataFolder(), "players.yml");
+
         if (!playerConfigFile.exists()) {
             playerConfigFile.getParentFile().mkdirs();
             saveResource("players.yml", false);
         }
+
         playerConfig = YamlConfiguration.loadConfiguration(playerConfigFile);
+    }
+
+    /* =========================
+       Getters
+       ========================= */
+
+    public static ProjectUnknown getInstance() {
+        return instance;
+    }
+
+    public RevealManager getRevealManager() {
+        return revealManager;
     }
 }
